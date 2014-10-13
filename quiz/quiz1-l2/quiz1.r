94248349046cf867bdd1b142bef989a1a8462128
Shiwei Chen <simon.c312@berkeley.edu>
2014-10-13 10:49:16 -0700

    quiz

--- quiz/quiz1-l2/quiz1.r
+++ quiz/quiz1-l2/quiz1.r
@@ -1,13 +1,13 @@
 # Please load in the dataset included in the quiz1-l2 directory. It will be
 # required to perform the following tasks. The dataset includes data for houses
 # in the city of Berkeley.
-
+load("SFHousing-2.rda")
 
 # calculate the mean and median bsqft of houses in Berkeley. Store these as the
 # variables <mean.bsqft> and <med.bsqft> respectively.
 
-# mean.bsft <- your code here
-# med.bsqft <- your code here
+mean.bsqft <- mean(housing[housing$city %in% c("Berkeley"), c("bsqft") ], na.rm = TRUE)
+med.bsqft <- median(housing[housing$city %in% c("Berkeley"), c("bsqft") ], na.rm = TRUE)
 
 
 # For each house in the dataset, calculate the squared difference between its
@@ -15,7 +15,8 @@
 # <bsqft.diffs>. Note that this should be a numeric vector with length equal to
 # the number of observations in the dataset
 
-# bsqft.diffs <- your code here
+bsqft.diffs <- sapply(housing[housing$city %in% c("Berkeley"), c("bsqft") ],
+                      function(obs){obs^2 - med.bsqft^2})
 
 
 
@@ -24,15 +25,15 @@
 # 1) houses whose bsqft is strictly greater than <mean.bsqft>:  <bsft.greater>
 # 2) houses whose bsqft is less than or equal to  <mean.bsqft>: <bsqft.less>
 
-# bsqft.greater <- your code here
-# bsqft.less <- your code here
+bsqft.greater <- housing[housing$bsqft > mean.bsqft,]
+bsqft.less <- housing[housing$bsqft <= mean.bsqft,]
 
 
 # For each of your subsets, create a vector giving the price of each house. Name
 # these variables <rooms.greater.price> and <rooms.less.price>.
 
-# rooms.greater.price <- your code here
-# rooms.less.price <- your code here
+rooms.greater.price <- bsqft.greater$price
+rooms.less.price <-  bsqft.less$price
 
 
 
@@ -49,8 +50,9 @@
 # <br> in the range (inclusive) specified by <room.range>
 
 priceByRooms <- function(room.range, br, prices) {
-
-    # your code here
+    df <- data.frame(br,prices)
+    colnames(df) <- c("br","prices")
+    return mean(df[(df$br >= room.range[1]) & (df$br <= room.range[2]),c("prices")])
 
 }
 
@@ -60,4 +62,11 @@ priceByRooms <- function(room.range, br, prices) {
 # 1) a title "Housing price vs Number of Rooms"
 # 2) axis labels: "price" and "number of rooms"
 # 3) plotting character set to 20
+
+plot(housing$price ~ housing$br,
+     data = housing, 
+     main = "Housing price vs Number of Rooms",
+     xlab = "number of rooms",
+     ylab = "price",
+     pch = 20, cex = 0.5)
 
